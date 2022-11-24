e9f2189a1b3738dbf61be69f4402dc9693e88f18
Esraa Shaaban <esraa_ali.94@yahoo.com>
2022-11-24 09:27:41 +0200

    Add Models b ..

---
+++ EFCore5Demo/Models/Blog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore5Demo.Models
+{
+	public class Blog
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public List<Post> Posts { get; set; }
+	}
+}
---
+++ EFCore5Demo/Models/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCore5Demo.Models
+{
+	public class Post
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public Blog Blog { get; set; }
+	}
+}
